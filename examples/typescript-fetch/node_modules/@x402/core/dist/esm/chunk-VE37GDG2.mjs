11f32232b7cd363e68fa00284689cbd78a540017
Vlad George Iftode <vladiftode3@gmail.com>
2026-08-13 14:58:04 +0200

    Exclude local TypeScript dependencies

--- examples/typescript-fetch/node_modules/@x402/core/dist/esm/chunk-VE37GDG2.mjs
+++
@@ -1,7 +0,0 @@
-// src/index.ts
-var x402Version = 2;
-
-export {
-  x402Version
-};
-//# sourceMappingURL=chunk-VE37GDG2.mjs.map
--- examples/typescript-fetch/node_modules/@x402/core/dist/esm/index.d.mts
+++
@@ -1,3 +0,0 @@
-declare const x402Version = 2;
-
-export { x402Version };
